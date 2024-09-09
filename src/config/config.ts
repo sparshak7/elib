209a26518d65296a7812d0a1b67c4904270792ac
Sparshak Nag <riley.onduty@gmail.com>
2024-09-09 19:01:50 +0530

    jwt

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ const _config = {
   port: process.env.PORT,
   dbUri: process.env.MONGO_URI,
   env: process.env.NODE_ENV,
+  jwt_secret: process.env.JWT_SECRET
 }
 
 export const config = Object.freeze(_config)
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from 'bcrypt'
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async(req: Request,res: Response,next: NextFunction) => {
   const {name, email, password} = req.body
@@ -21,7 +23,9 @@ const createUser = async(req: Request,res: Response,next: NextFunction) => {
   const hashedPassword = await bcrypt.hash(password, 10)
   const newUser = await userModel.create({name, email, password: hashedPassword})
 
-  res.json({_id: newUser._id, message: "User created."})
+  const token = sign({sub: newUser._id}, config.jwt_secret as string, {expiresIn: "7d"})
+
+  res.json({access_token: token, message: "User created."})
 
 }
 
