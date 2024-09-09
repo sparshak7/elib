b78acbdf7e8a89e903798f15eba4c1f7f0890bf0
Sparshak Nag <riley.onduty@gmail.com>
2024-09-09 13:22:35 +0530

    added mongo

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import app from "./src/app"
 import { config } from "./src/config/config"
+import connectDB from "./src/config/db"
 // process.loadEnvFile()
 
-const startServer = () => {
+const startServer = async() => {
+  await connectDB()
   const port = config.port || 3000
   app.listen(port, () => {
     console.log(`Server started on port ${port}`)
--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,8 @@ import { config as conf } from "dotenv"
 conf()
 
 const _config = {
-  port: process.env.PORT
+  port: process.env.PORT,
+  dbUri: process.env.MONGO_URI
 }
 
 export const config = Object.freeze(_config)
